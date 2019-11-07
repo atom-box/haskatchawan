402745fa2e9964c0918ea8f65f001019b43598e4
Evan Genest <genest.1@osu.edu>
2019-11-07 07:34:05 -0600

    Works.

--- src1/manumissionabolition.hs
+++ src1/manumissionabolition.hs
@@ -1,5 +1,9 @@
+{- solve pythagorean theorem -}
+
 main = do
-       putStrLn "enter value for x: "
-       input1 <- getLine
-       let x = (read input1 :: Int)
-       print (x)
+       putStrLn "Enter maximum hypotenuse length: "
+       hyp <- getLine
+       let z = (read hyp :: Int)
+       let triangles = [[x, y, z] | x <- [1..100], y <-[1..100], z <- [1..z], z^2 == y^2 + x^2 ]
+       print (triangles)
+

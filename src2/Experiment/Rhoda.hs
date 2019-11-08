28ee587a87e6a521acb3f84ca11842764ec92287
Evan Genest <genest.1@osu.edu>
2019-11-08 12:42:21 -0600

    Make 5, 3 work, and convert Int to String in Fizzer.

--- src2/Experiment/Rhoda.hs
+++ src2/Experiment/Rhoda.hs
@@ -1,5 +1,6 @@
 module Experiment.Rhoda 
 (sinister 
+, fizzer
 ) where
 
 
@@ -12,4 +13,15 @@ sinister s = do
   let width = div (length s) 2
   take width s
 
+{-
+Print out all the numbers from 1 to 100. But for every number divisible by 3 print replace it with the word “Fizz,” for any number divisible by 5 replace it with the word “Buzz” and for a number divisible by both 3 and 5 replace it with the word “FizzBuzz.”
+-}
+fizzer n = if (mod n 5 == 0 && mod n 3 == 0)
+  then "BizzFuzz"
+  else show n
 
+
+
+{-
+Implment SLICE from JS manually
+-}

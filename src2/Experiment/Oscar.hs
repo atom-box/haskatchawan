1b7b496674378f12f67209266ab4acc075b31ec3
Evan Genest <genest.1@osu.edu>
2019-11-08 07:41:57 -0600

    Add char replacer. Add annotation to output.

--- src2/Experiment/Oscar.hs
+++ src2/Experiment/Oscar.hs
@@ -4,9 +4,29 @@ I WROTE THIS CODE.
 
 module Experiment.Oscar  
 ( fibonacci
+, bunnyEars 
+, x2y
 ) where
 
-
+{-
+Recursive Fibonacci.
+-}
 fibonacci 0 = 1
 fibonacci 1 = 1
 fibonacci n = fibonacci (n -1) + fibonacci (n-2)
+
+
+{-
+Recursive bunny ears.
+We have a number of bunnies and each bunny has two big floppy ears. We want 
+to compute the total number of ears across all the bunnies recursively 
+(without loops or multiplication).
+-}
+bunnyEars 0 = 0
+bunnyEars 1 = 2
+bunnyEars b = 2 + bunnyEars (b - 1)
+
+{-
+Given a string, compute recursively (no loops) a new string where all the lowercase 'x' chars have been changed to 'y' chars.
+-}
+x2y str = [if x == 'x' then 'y' else x | x <- str ]
--- src2/Main.hs
+++ src2/Main.hs
@@ -7,13 +7,21 @@ import Experiment.Oscar
 
 
 main = do
-  putStrLn(lucky 7)
-  putStrLn(sayMe 3)
+  putStrLn "Recursive Fibonacci: "
   print(fibonacci 2)
   print(fibonacci 3)
   print(fibonacci 4)
   print(fibonacci 5)
   print(fibonacci 6)
+  putStrLn "Recursive BunnyEars: "
+  print(bunnyEars 6)
+  print(bunnyEars 33)
+  putStrLn "Recursive char replacement of x with y: "
+  let slogan = "Excellency, Your Xerxes!"
+  putStrLn slogan
+  print(x2y slogan)
+
+
 
 {-
 http://learnyouahaskell.com/modules
